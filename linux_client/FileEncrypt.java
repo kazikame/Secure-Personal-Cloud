778b2e1cd0a0aa3fe5f672bb4f14b11949515e16
yash-s20 <yash.sharma200999@gmail.com>
2018-10-28 15:40:30 +0530

    Complete working implementation of AES and TripleDES with references

--- linux_client/FileEncrypt.java
+++ linux_client/FileEncrypt.java
@@ -4,6 +4,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 //reference: https://javapapers.com/java/java-symmetric-aes-encryption-decryption-using-jce/
@@ -11,10 +12,10 @@ public class FileEncrypt {
     public static void main(String... args) throws IOException {
         /*
          *  args[0]: File Encryption Schema
-         *  args[1]: "encrypt"(/"decrypt")
+         *  args[1]: "encrypt"/"decrypt"
          *  args[2]: decrypted key
-         *  args[3]: file path of encrypted/decrypted file to decrypt/encrypt
-         *  args[4]: file path to store decrypted/encrypted file
+         *  args[3]: file path of decrypted/encrypted file to encrypt/decrypt
+         *  args[4]: file path to store encrypted/decrypted file
          */
         FileInputStream keyf = null;
         FileInputStream fin = null;
@@ -32,9 +33,15 @@ public class FileEncrypt {
                 if (args[0].equals("AES")) {
                     AESEncrypt(keyf, fin, fos);
                 }
-            } else if (args[1].equals("decrypt")) {
+                else if(args[0].equals("TripleDES")) {
+                    DESedeEncrypt(keyf, fin, fos);
+                }
+            }else if (args[1].equals("decrypt")) {
                 if (args[0].equals("AES")) {
-                    AESDecrypt(keyf,fin,fos);
+                    AESDecrypt(keyf, fin, fos);
+                }
+                else if(args[0].equals("TripleDES")) {
+                    DESedeDecrypt(keyf, fin, fos);
                 }
             }
         }
@@ -43,13 +50,14 @@ public class FileEncrypt {
         }
     }
 
-    private static void AESEncrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, IOException, InvalidKeyException {
-        Cipher cipher = Cipher.getInstance("AES");
-        byte[] key = new byte[16];
+    private static void DESedeEncrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] fileByteArray = new byte[fileInputStream.available()];
+        Cipher cipher = Cipher.getInstance("DESede");
+        byte[] key = new byte[24];
         keyFileInputStream.read(key);
-        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        SecretKey secretKey = new SecretKeySpec(key, "DESede");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        byte[] plainTextByte = new byte[32];
+        byte[] plainTextByte = new byte[48];
         int bytesRead;
         while ((bytesRead = fileInputStream.read(plainTextByte)) != -1) {
             byte[] output = cipher.update(plainTextByte, 0, bytesRead);
@@ -57,12 +65,84 @@ public class FileEncrypt {
                 fileOutputStream.write(output);
             }
         }
+        byte[] output = cipher.doFinal();
+        if (output != null)
+            fileOutputStream.write(output);
+
+        fileInputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        System.out.println("File encrypted.");
     }
-    private static void AESDecrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException {
+
+    private static void DESedeDecrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws IOException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] fileByteArray = new byte[fileInputStream.available()];
+        Cipher cipher = Cipher.getInstance("DESede");
+        byte[] key = new byte[24];
+        keyFileInputStream.read(key);
+        SecretKey secretKey = new SecretKeySpec(key, "DESede");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] in = new byte[48];
+        int read;
+        while ((read = fileInputStream.read(in)) != -1) {
+            byte[] output = cipher.update(in, 0, read);
+            if (output != null)
+                fileOutputStream.write(output);
+        }
+
+        byte[] output = cipher.doFinal();
+        if (output != null)
+            fileOutputStream.write(output);
+        fileInputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
+        System.out.println("File decrypted.");
+    }
+
+    private static void AESEncrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException, IOException, InvalidKeyException {
         Cipher cipher = Cipher.getInstance("AES");
-        byte[] key = new byte[16];
+        byte[] key = new byte[32];
         keyFileInputStream.read(key);
         SecretKey secretKey = new SecretKeySpec(key, "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] plainTextByte = new byte[64];
+        int bytesRead;
+        while ((bytesRead = fileInputStream.read(plainTextByte)) != -1) {
+            byte[] output = cipher.update(plainTextByte, 0, bytesRead);
+            if (output != null) {
+                fileOutputStream.write(output);
+            }
+        }
+        byte[] output = cipher.doFinal();
+        if (output != null)
+            fileOutputStream.write(output);
+
+        fileInputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
+//        System.out.println("File encrypted.");
+    }
+    private static void AESDecrypt(FileInputStream keyFileInputStream, FileInputStream fileInputStream, FileOutputStream fileOutputStream) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        Cipher cipher = Cipher.getInstance("AES");
+        byte[] key = new byte[32];
+        keyFileInputStream.read(key);
+        SecretKey secretKey = new SecretKeySpec(key, "AES");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] in = new byte[64];
+        int read;
+        while ((read = fileInputStream.read(in)) != -1) {
+            byte[] output = cipher.update(in, 0, read);
+            if (output != null)
+                fileOutputStream.write(output);
+        }
+
+        byte[] output = cipher.doFinal();
+        if (output != null)
+            fileOutputStream.write(output);
+        fileInputStream.close();
+        fileOutputStream.flush();
+        fileOutputStream.close();
+//        System.out.println("File Decrypted.");
+
     }
 }
--- linux_client/GenerateKey.java
+++ linux_client/GenerateKey.java
@@ -1,12 +1,16 @@
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 
 public class GenerateKey {
+    //aes key length = 256
+    //tdes key length = 192 (although the real thing will be 168 inside due to padding)
     public static void main(String... args) {
         /*
          * args[0] : encryption schema
@@ -33,15 +37,26 @@ public class GenerateKey {
     }
     private static void GenerateAES(FileOutputStream fileOutputStream) throws NoSuchAlgorithmException, IOException {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-        keyGenerator.init(128);
+        keyGenerator.init(256);
         SecretKey secretKey = keyGenerator.generateKey();
         byte[] key = secretKey.getEncoded();
+//        System.out.println(key.length);
         fileOutputStream.write(key);
     }
     private static void GenerateDESede(FileOutputStream fileOutputStream) throws NoSuchAlgorithmException, IOException {
-        KeyGenerator keyGenerator = KeyGenerator.getInstance("Desede");
-        keyGenerator.init(168);
-        SecretKey secretKey = keyGenerator.generateKey();
+//        KeyGenerator keyGenerator = KeyGenerator.getInstance("Desede");
+//        keyGenerator.init(192);
+//        SecretKey secretKey = keyGenerator.generateKey();
+//        byte[] key = secretKey.getEncoded();
+//        fileOutputStream.write(key);
+        /*
+        reference: https://hamzakc.wordpress.com/2006/12/29/create-triple-des-secretkey-in-java/
+        although i don't know why not the above commented out code, weird.
+         */
+
+        byte[] randomBytes = new byte[24];
+        new Random().nextBytes(randomBytes);
+        SecretKey secretKey = new SecretKeySpec(randomBytes, "DESede");
         byte[] key = secretKey.getEncoded();
         fileOutputStream.write(key);
     }
