c45034785132c734de1f54b1222ee0b415d38f4d
Parikshit Bansal <34788639+pbansal5@users.noreply.github.com>
2018-11-21 18:52:20 +0530

    Update daemon.py

--- linux_client/daemon.py
+++ linux_client/daemon.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import os, time
-from conflicts import uploadall
 from startup import *
 
 def Daemon():
@@ -14,23 +13,11 @@ def Daemon():
             os._exit(0)
             
     except OSError, error:
-        print 'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
+        print ('Unable to fork. Error: %d (%s)' % (error.errno, error.strerror))
         os._exit(1)
         
     while (True):
-        cloud_dict = get_cloud_dict();
-        local_dir = get_local_dir();
-        [upload,download,delete] = uploadall(startup.get_index(startup.server_url, startup.AuthKey), startup.home_dir)
-        except requests.exceptions.ConnectionError as e:
-        logging.exception(e)
-        print("error: The server isn't responding. To change/set the server url, use\n\nspc server set_url <url:port>")
-        exit(-1)
-    except NoHomeDirException as e:
-        logging.exception(e)
-        print("error: Invalid home directory. Please point to a valid home directory using\n\nspc observe <home-dir>")
-        exit(-1)
-        startup.upload_files(startup.server_url,startup.AuthKey,startup.home_dir,upload)
-        startup.delete_files(startup.server_url,startup.AuthKey,startup.home_dir,delete)
+        startup.uploadlocal();
         sleep(86400)
 
 Daemon()
