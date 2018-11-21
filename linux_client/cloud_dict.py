7d696a8d5cb9db8969feba5555ac80dc395f4914
pbansal5 <parikshitb52@gmail.com>
2018-11-21 16:03:35 +0530

    daemon.py

---
+++ linux_client/cloud_dict.py
@@ -0,0 +1,8 @@
+def get_cloud_dict():   # return the cloud_dict used in conflicts.py
+    pass
+
+def get_local_dir():   # return the local_dir path
+    pass
+
+def upload([upload,download,delete]):  # the upload function
+    pass
---
+++ linux_client/daemon.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os, time
+from conflicts import uploadall
+from cloud_dict import get_cloud_dict, get_local_dict,upload
+
+def Daemon():
+
+    try:
+        pid = os.fork()
+        
+        if pid > 0:
+            os._exit(0)
+            
+    except OSError, error:
+        print 'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
+        os._exit(1)
+
+    cloud_dict = get_cloud_dict();
+    local_dir = get_local_dir();
+    upload(uploadall(cloud_dict,local_dir))
+
+Daemon()
