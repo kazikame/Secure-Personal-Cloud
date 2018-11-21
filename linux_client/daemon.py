7987002ea13885c3b2f349d650dc2cbe03a92c91
yash-s20 <yash.sharma200999@gmail.com>
2018-11-21 17:34:54 +0530

    Fixed syntax on daemon.py

--- linux_client/daemon.py
+++ linux_client/daemon.py
@@ -3,7 +3,11 @@
 
 import os, time
 from conflicts import uploadall
-from startup import *
+from django.contrib.sites import requests
+from pip.utils import logging
+
+import linux_client.startup as startup
+
 
 def Daemon():
 
@@ -11,26 +15,29 @@ def Daemon():
         pid = os.fork()
         
         if pid > 0:
-            os._exit(0)
+            exit(0)
             
-    except OSError, error:
-        print 'Unable to fork. Error: %d (%s)' % (error.errno, error.strerror)
-        os._exit(1)
-        
-    while (True):
-        cloud_dict = get_cloud_dict();
-        local_dir = get_local_dir();
-        [upload,download,delete] = uploadall(startup.get_index(startup.server_url, startup.AuthKey), startup.home_dir)
-        except requests.exceptions.ConnectionError as e:
-        logging.exception(e)
-        print("error: The server isn't responding. To change/set the server url, use\n\nspc server set_url <url:port>")
-        exit(-1)
-    except NoHomeDirException as e:
+    except OSError as error:
+        print('Unable to fork. Error: %d (%s)' % (error.errno, error.strerror))
+        exit(1)
+
+    try:
+
+        while (True):
+            try:
+                cloud_dict = get_cloud_dict();
+                local_dir = get_local_dir();
+                [upload,download,delete] = uploadall(startup.get_index(startup.server_url, startup.AuthKey), startup.home_dir)
+            except requests.exceptions.ConnectionError as e:
+                logging.exception(e)
+                print("error: The server isn't responding. To change/set the server url, use\n\nspc server set_url <url:port>")
+                exit(-1)
+    except startup.NoHomeDirException as e:
         logging.exception(e)
         print("error: Invalid home directory. Please point to a valid home directory using\n\nspc observe <home-dir>")
         exit(-1)
         startup.upload_files(startup.server_url,startup.AuthKey,startup.home_dir,upload)
         startup.delete_files(startup.server_url,startup.AuthKey,startup.home_dir,delete)
-        sleep(86400)
+        time.sleep(86400)
 
 Daemon()
