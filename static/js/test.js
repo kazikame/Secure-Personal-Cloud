c96899766678d1a2301065bc5a591c68de359b47
yash-s20 <yash.sharma200999@gmail.com>
2018-11-21 23:26:03 +0530

    AES possible now

--- static/js/test.js
+++ static/js/test.js
@@ -1,24 +1,17 @@
 var salt = "E612A048FE0BCE53";
 var key = CryptoJS.enc.Hex.parse("2AE7B9111B2E116F2A15224BEF05BBCB17379E90261232DDDE2C898F08AD28DA");
 var iv = CryptoJS.enc.Hex.parse("FE9AE37315E8B368E7BB5B5732E57645");
-var message = "blah ko encrypt kiya";
-var encrypted = CryptoJS.AES.encrypt(message, key, {iv: iv});
-console.log(encrypted.toString());
-var decrypted = CryptoJS.AES.decrypt(encrypted,key, {iv: iv,padding: CryptoJS.pad.Pkcs7, mode: CryptoJS.mode.CBC});
-console.log(decrypted.toString(CryptoJS.enc.Utf8));
-encrypted = CryptoJS.enc.Base64.parse("iN42vonhIL+auH6OfxVGo8lwBVkdcogGXBoye2j3XWQ=");
-console.log(encrypted.toString());
-decrypted = CryptoJS.AES.decrypt(encrypted, key, {iv: iv,padding: CryptoJS.pad.Pkcs7, mode: CryptoJS.mode.CBC});
+var message = "hi this is yash";
+// var encrypted = CryptoJS.AES.encrypt(message, key, {iv: iv, mode: CryptoJS.mode.CTR});
+// console.log(encrypted.toString());
+var enmsg = "yM9NwHtZfCTEHxXYt3qpGA==";
+var decrypted = CryptoJS.AES.decrypt(
+    enmsg,
+    key,
+    {
+        iv: iv,
+        mode:CryptoJS.mode.CTR,
+        padding: CryptoJS.pad.Pkcs7
+    });
 console.log(decrypted.toString(CryptoJS.enc.Utf8));
 
-function encrypt(){
-    var encrypted = CryptoJS.AES.encrypt(document.getElementById("text").value, document.getElementById("pass").value);
-    document.getElementById("result").innerHTML = encrypted;
-    document.getElementById("decrypted").innerHTML = '';
-}
-
-function decrypt(){
-    var decrypted = CryptoJS.AES.decrypt(document.getElementById("result").innerHTML, document.getElementById("pass").value).toString(CryptoJS.enc.Utf8);
-    document.getElementById("decrypted").innerHTML = decrypted;
-    document.getElementById("result").innerHTML = '';
-}
