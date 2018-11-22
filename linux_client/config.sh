497de34698a064a9a17938007ce14bc9e36fba42
saksham.goel <saksham.goel@iitb.ac.in>
2018-11-22 20:06:09 +0530

    minor changes

--- linux_client/config.sh
+++ linux_client/config.sh
@@ -6,23 +6,22 @@ observe="observe"
 sync="sync"
 daemon="stop_daemon"
 setkey="set_key"
-dic_file="conf.json"
 
 # cd $SPC_PATH
 
 if [ "$1" == "$server" ]
 then
-    python3.6 startup.py empty_json "$dic_file"
-    python3.6 startup.py set_server "$2" "$dic_file"
+    python3.6 startup.py empty_json
+    python3.6 startup.py set_server "$2"
 elif [ "$1" == "$config" ]
 then
-    python3.6 startup.py config "$dic_file"
+    python3.6 startup.py config
 elif [ "$1" == "$observe" ]
 then
-    python3.6 startup.py observe "$2" "$dic_file"
+    python3.6 startup.py observe "$2"
 elif [ "$1" == "$sync" ]
 then
-    python3.6 startup.py sync "$dic_file"
+    python3.6 startup.py sync
 elif [ "$1" == "$daemon" ]
 then
     pid=$(ps -aux | grep "daemon.py$" | sed 's/\s\+/ /g' | cut -d' ' -f2)
